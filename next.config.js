cfd8187bc5a990db0ba6af6dbb7ad31487647b75
shkim <shkim@ibleaders.co.kr>
2025-10-06 23:57:06 +0900

    add Action file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
+  basePath: process.env.NODE_ENV  === 'production' ? '/7to8converter2' : '',
+  assetPrefix: process.env.NODE_ENV  === 'production' ? '/7to8converter2/' : '',
 }
 
 module.exports = nextConfig
